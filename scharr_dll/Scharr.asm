0f2e89253405a0b90942237b86ea945034714c7d
Samuel Ujazda <91287013+LuXRocK@users.noreply.github.com>
2025-02-12 17:54:52 +0100

    Add files via upload

--- scharr_dll/Scharr.asm
+++ scharr_dll/Scharr.asm
@@ -1,236 +1,252 @@
-; scharr_dll.asm
-; Kompilujemy w trybie x64 (Microsoft x64 calling convention)
-.CODE
+option casemap:none
 
+; Upublicznienie funkcji, aby była widoczna poza tym modułem
 PUBLIC ASMScharrFunction
 
-; Parametry:
-;   RCX = pointer do bufora wej�ciowego (inputImage)
-;   RDX = pointer do bufora wyj�ciowego (outputImage)
-;   R8  = width (szeroko�� obrazu)
-;   R9  = height (wysoko�� obrazu)
+.code
 ASMScharrFunction PROC
-    ; Zachowujemy rejestry, kt�re b�dziemy u�ywa�
-    push rsi
-    push rdi
-    push rbx
-    push r12
-    push r13
-    push r14
-    push r15
-    ; Zachowujemy dodatkowo width i height � p�niej je pobierzemy
-    push r8      ; width
-    push r9      ; height
+    ; -------------------- PROLOG --------------------
+    push    rbp             ; Zapisujemy poprzedni wskaźnik ramki stosu
+    mov     rbp, rsp        ; Ustawiamy nowy wskaźnik ramki stosu na bieżący wskaźnik stosu
+    push    rbx             ; Zapisujemy rejestr RBX (zachowujemy jego wartość)
+    push    rsi             ; Zapisujemy rejestr RSI (zachowujemy jego wartość)
+    push    rdi             ; Zapisujemy rejestr RDI (zachowujemy jego wartość)
+    push    r12             ; Zapisujemy rejestr R12 (zachowujemy jego wartość)
+    push    r13             ; Zapisujemy rejestr R13 (zachowujemy jego wartość)
+    push    r14             ; Zapisujemy rejestr R14 (zachowujemy jego wartość)
+    push    r15             ; Zapisujemy rejestr R15 (zachowujemy jego wartość)
+    sub     rsp, 64         ; Rezerwujemy 64 bajty na zmienne lokalne (obszar roboczy)
 
-    ; Skopiuj width do EDI (32-bit)
-    mov edi, r8d
+    ; ----------------- USTAWIENIE PARAMETRÓW -----------------
+    ; Zgodnie z konwencją Microsoft x64:
+    ; RCX = wskaźnik do obrazu wejściowego (inputImage)
+    ; RDX = wskaźnik do obrazu wyjściowego (outputImage)
+    ; R8  = szerokość obrazu (width)
+    ; R9  = wysokość obrazu (height)
+    mov     rsi, rcx        ; Przenosimy wskaźnik inputImage do rejestru RSI
+    mov     rdi, rdx        ; Przenosimy wskaźnik outputImage do rejestru RDI
+    mov     rbx, r8         ; Przechowujemy szerokość obrazu w rejestrze RBX
+    mov     r10, r9         ; Przechowujemy wysokość obrazu w rejestrze R10
 
-    ; Sprawd� minimalne wymiary obrazu (musi by� co najmniej 3x3)
-    cmp r8d, 3
-    jl ExitASM
-    cmp r9d, 3
-    jl ExitASM
+    ; -------------------- CZYSZCZENIE KRAWĘDZI --------------------
+    ; Zerujemy górny wiersz obrazu wyjściowego (pierwszy wiersz)
+    xor     rax, rax        ; Ustawiamy licznik kolumn (RAX) na 0
+top_row_loop:
+    cmp     rax, rbx        ; Porównujemy licznik z szerokością obrazu
+    jge     top_row_done    ; Jeśli osiągnęliśmy szerokość, kończymy pętlę
+    mov     byte ptr [rdi + rax], 0  ; Ustawiamy piksel w górnym wierszu na 0
+    inc     rax             ; Zwiększamy licznik kolumn
+    jmp     top_row_loop    ; Powtarzamy pętlę
+top_row_done:
 
-    ; p�tla po wierszach � pomijamy pierwszy i ostatni (obrze�a)
-    mov r10d, 1          ; current y = 1
-    mov r11d, r9d
-    sub r11d, 1          ; y_max = height - 1
+    ; Zerujemy dolny wiersz obrazu wyjściowego (ostatni wiersz)
+    mov     rax, r10        ; Ładujemy wysokość obrazu
+    dec     rax             ; Obliczamy indeks ostatniego wiersza (height - 1)
+    imul    rax, rbx        ; Przemnażamy indeks przez szerokość, aby uzyskać offset w pamięci
+    lea     r11, [rdi + rax] ; Obliczamy adres początku dolnego wiersza i zapisujemy go w R11
+    xor     rax, rax        ; Resetujemy licznik kolumn
+bottom_row_loop:
+    cmp     rax, rbx        ; Sprawdzamy, czy przetworzyliśmy wszystkie kolumny
+    jge     bottom_row_done ; Jeśli tak, wychodzimy z pętli
+    mov     byte ptr [r11 + rax], 0  ; Ustawiamy piksel w dolnym wierszu na 0
+    inc     rax             ; Zwiększamy licznik kolumn
+    jmp     bottom_row_loop ; Powtarzamy pętlę
+bottom_row_done:
 
-YLoop:
-    cmp r10d, r11d
-    jge LoopEnd
+    ; Zerujemy lewą kolumnę obrazu wyjściowego (pierwsza kolumna każdej linii)
+    xor     rax, rax        ; Ustawiamy licznik wierszy na 0
+left_col_loop:
+    cmp     rax, r10        ; Sprawdzamy, czy przetworzyliśmy wszystkie wiersze
+    jge     left_col_done   ; Jeśli tak, kończymy pętlę
+    mov     rcx, rax        ; Kopiujemy bieżący indeks wiersza do RCX
+    imul    rcx, rbx        ; Obliczamy offset wiersza (wiersz * szerokość)
+    mov     byte ptr [rdi + rcx], 0 ; Ustawiamy pierwszy piksel w danym wierszu (lewa kolumna) na 0
+    inc     rax             ; Zwiększamy licznik wierszy
+    jmp     left_col_loop   ; Powtarzamy pętlę
+left_col_done:
 
-    ; p�tla po kolumnach � pomijamy pierwszy i ostatni
-    mov r12d, 1          ; current x = 1
-    mov r13d, r8d
-    sub r13d, 1          ; x_max = width - 1
+    ; Zerujemy prawą kolumnę obrazu wyjściowego (ostatnia kolumna każdej linii)
+    xor     rax, rax        ; Ustawiamy licznik wierszy na 0
+right_col_loop:
+    cmp     rax, r10        ; Sprawdzamy, czy przetworzyliśmy wszystkie wiersze
+    jge     right_col_done  ; Jeśli tak, kończymy pętlę
+    mov     rcx, rax        ; Kopiujemy bieżący indeks wiersza do RCX
+    imul    rcx, rbx        ; Obliczamy offset wiersza (wiersz * szerokość)
+    mov     rdx, rbx        ; Ładujemy szerokość obrazu do RDX
+    dec     rdx             ; Obliczamy indeks ostatniej kolumny (width - 1)
+    add     rcx, rdx        ; Dodajemy indeks ostatniej kolumny, aby uzyskać offset ostatniego piksela w wierszu
+    mov     byte ptr [rdi + rcx], 0 ; Ustawiamy ten piksel na 0
+    inc     rax             ; Zwiększamy licznik wierszy
+    jmp     right_col_loop  ; Powtarzamy pętlę
+right_col_done:
 
-XLoop:
-    cmp r12d, r13d
-    jge XLoopEnd
+    ; ----------------- PRZETWARZANIE PIKSELI WEWNĄTRZ OBRAZU -----------------
+    ; Przetwarzamy wiersze od 1 do (height - 2) – pomijamy krawędzie
+    mov     r11, 1         ; Ustawiamy indeks bieżącego wiersza na 1 (pomijamy pierwszy wiersz)
+outer_loop:
+    mov     rax, r10
+    dec     rax             ; Obliczamy indeks ostatniego wiersza (height - 1)
+    cmp     r11, rax        ; Jeśli bieżący wiersz >= ostatni wiersz, kończymy przetwarzanie
+    jge     end_outer_loop
 
-    ; Zerujemy sumy dla kierunk�w X i Y
-    xor r14d, r14d       ; sumX = 0
-    xor r15d, r15d       ; sumY = 0
+    ; Obliczamy adresy trzech wierszy obrazu wejściowego:
+    ; 1) Wiersz powyżej bieżącego (r11 - 1)
+    mov     rax, r11
+    dec     rax             ; rax = r11 - 1
+    imul    rax, rbx        ; rax = (r11 - 1) * width
+    add     rax, rsi        ; rax = adres początku wiersza powyżej
+    mov     qword ptr [rbp - 8], rax   ; Zapisujemy adres w lokalnej zmiennej (top row)
 
-    ; ---------------------------
-    ; Przetwarzamy 8 s�siad�w:
-    ; S�siedzi i ich wagi:
-    ; (-1,-1): weightX = 3,   weightY = 3
-    ; (-1, 0): weightX = 0,   weightY = 10
-    ; (-1, 1): weightX = -3,  weightY = 3
-    ; ( 0,-1): weightX = 10,  weightY = 0
-    ; ( 0, 1): weightX = -10, weightY = 0
-    ; ( 1,-1): weightX = 3,   weightY = -3
-    ; ( 1, 0): weightX = 0,   weightY = -10
-    ; ( 1, 1): weightX = -3,  weightY = -3
-    ; ---------------------------
+    ; 2) Bieżący wiersz (r11)
+    mov     rax, r11
+    imul    rax, rbx        ; rax = r11 * width
+    add     rax, rsi        ; rax = adres początku bieżącego wiersza
+    mov     qword ptr [rbp - 16], rax  ; Zapisujemy adres bieżącego wiersza
 
-    ; --- S�siad (-1, -1) ---
-    mov eax, r10d
-    add eax, -1           ; y + (-1)
-    imul eax, edi         ; (y-1)*width
-    mov ecx, r12d
-    add ecx, -1           ; x + (-1)
-    add eax, ecx          ; index = (y-1)*width + (x-1)
-    movzx edx, byte ptr [rsi+rax] ; edx = pixel
-    ; sumX += pixel * 3
-    imul edx, 3
-    add r14d, edx
-    ; sumY += pixel * 3
-    movzx edx, byte ptr [rsi+rax]
-    imul edx, 3
-    add r15d, edx
+    ; 3) Wiersz poniżej bieżącego (r11 + 1)
+    mov     rax, r11
+    inc     rax             ; rax = r11 + 1
+    imul    rax, rbx        ; rax = (r11 + 1) * width
+    add     rax, rsi        ; rax = adres początku wiersza poniżej
+    mov     qword ptr [rbp - 24], rax  ; Zapisujemy adres dolnego wiersza
 
-    ; --- S�siad (-1, 0) ---
-    mov eax, r10d
-    add eax, -1
-    imul eax, edi
-    mov ecx, r12d
-    ; x + 0
-    add eax, ecx
-    movzx edx, byte ptr [rsi+rax]
-    ; sumX += pixel * 0 (pomijamy)
-    ; sumY += pixel * 10
-    imul edx, 10
-    add r15d, edx
+    ; Obliczamy adres bieżącego wiersza obrazu wyjściowego
+    mov     rax, r11
+    imul    rax, rbx        ; rax = r11 * width
+    add     rax, rdi        ; rax = adres początku bieżącego wiersza w obrazie wyjściowym
+    mov     qword ptr [rbp - 32], rax  ; Zapisujemy ten adres w zmiennej lokalnej
 
-    ; --- S�siad (-1, 1) ---
-    mov eax, r10d
-    add eax, -1
-    imul eax, edi
-    mov ecx, r12d
-    add ecx, 1
-    add eax, ecx
-    movzx edx, byte ptr [rsi+rax]
-    ; sumX += pixel * -3
-    imul edx, -3
-    add r14d, edx
-    ; sumY += pixel * 3
-    movzx edx, byte ptr [rsi+rax]
-    imul edx, 3
-    add r15d, edx
+    push    r11             ; Zachowujemy bieżący indeks wiersza na stosie
 
-    ; --- S�siad (0, -1) ---
-    mov eax, r10d
-    ; y + 0
-    imul eax, edi
-    mov ecx, r12d
-    add ecx, -1
-    add eax, ecx
-    movzx edx, byte ptr [rsi+rax]
-    ; sumX += pixel * 10
-    imul edx, 10
-    add r14d, edx
-    ; sumY += pixel * 0
+    ; ----------------- PRZETWARZANIE KOLUMN W BIEŻĄCYM WIERSZU -----------------
+    ; Przetwarzamy piksele od kolumny 1 do (width - 2) (pomijamy kolumny krawędziowe)
+    mov     r12, 1         ; Ustawiamy indeks kolumny na 1
+inner_loop_start:
+    mov     rax, rbx
+    dec     rax             ; rax = width - 1 (ostatnia kolumna, którą pomijamy)
+    cmp     r12, rax        ; Jeśli aktualna kolumna >= (width - 1), kończymy pętlę wewnętrzną
+    jge     end_inner_loop
 
-    ; --- S�siad (0, 1) ---
-    mov eax, r10d
-    imul eax, edi
-    mov ecx, r12d
-    add ecx, 1
-    add eax, ecx
-    movzx edx, byte ptr [rsi+rax]
-    ; sumX += pixel * -10
-    imul edx, -10
-    add r14d, edx
-    ; sumY += pixel * 0
+    ; Inicjalizacja akumulatorów na gradienty:
+    ; [rbp - 48] – pomocnicza zmienna (przechowuje indeks kolumny, dla odniesienia)
+    ; [rbp - 40] – akumulator dla poziomego gradientu (Gx)
+    ; [rbp - 44] – akumulator dla pionowego gradientu (Gy)
+    mov     dword ptr [rbp - 48], r12d ; Zapisujemy bieżący indeks kolumny (jako 32-bit)
+    mov     dword ptr [rbp - 40], 0    ; Zerujemy akumulator Gx
+    mov     dword ptr [rbp - 44], 0    ; Zerujemy akumulator Gy
 
-    ; --- S�siad (1, -1) ---
-    mov eax, r10d
-    add eax, 1
-    imul eax, edi
-    mov ecx, r12d
-    add ecx, -1
-    add eax, ecx
-    movzx edx, byte ptr [rsi+rax]
-    ; sumX += pixel * 3
-    imul edx, 3
-    add r14d, edx
-    ; sumY += pixel * -3
-    movzx edx, byte ptr [rsi+rax]
-    imul edx, -3
-    add r15d, edx
+    ; ----------------- PRZETWARZANIE SĄSIEDZTWA – WIERSZ GÓRNY -----------------
+    ; Lewy górny piksel (wiersz powyżej, kolumna r12 - 1)
+    mov     r13, qword ptr [rbp - 8]   ; Ładujemy adres wiersza powyżej (top row)
+    mov     r14, r12
+    dec     r14             ; Obliczamy indeks kolumny dla lewego sąsiada
+    movzx   r15d, byte ptr [r13 + r14] ; Wczytujemy wartość piksela (rozszerzając do 32 bitów)
+    imul    r15d, 3         ; Mnożymy wartość przez 3 (waga Scharr dla lewego górnego piksela)
+    add     dword ptr [rbp - 40], r15d ; Dodajemy do akumulatora Gx
+    add     dword ptr [rbp - 44], r15d ; Dodajemy do akumulatora Gy
 
-    ; --- S�siad (1, 0) ---
-    mov eax, r10d
-    add eax, 1
-    imul eax, edi
-    mov ecx, r12d
-    add eax, ecx
-    movzx edx, byte ptr [rsi+rax]
-    ; sumX += pixel * 0
-    ; sumY += pixel * -10
-    imul edx, -10
-    add r15d, edx
+    ; Górny środkowy piksel (wiersz powyżej, kolumna r12)
+    mov     r13, qword ptr [rbp - 8]   ; Ponownie adres wiersza powyżej
+    movzx   r15d, byte ptr [r13 + r12] ; Wczytujemy wartość piksela w kolumnie r12
+    imul    r15d, 5         ; Mnożymy przez 5 (waga Scharr dla górnego środkowego piksela)
+    add     dword ptr [rbp - 44], r15d ; Dodajemy do akumulatora Gy
+    add     dword ptr [rbp - 44], r15d ; Dodajemy wynik jeszcze raz (efektywnie 5 * 2)
 
-    ; --- S�siad (1, 1) ---
-    mov eax, r10d
-    add eax, 1
-    imul eax, edi
-    mov ecx, r12d
-    add ecx, 1
-    add eax, ecx
-    movzx edx, byte ptr [rsi+rax]
-    ; sumX += pixel * -3
-    imul edx, -3
-    add r14d, edx
-    ; sumY += pixel * -3
-    movzx edx, byte ptr [rsi+rax]
-    imul edx, -3
-    add r15d, edx
+    ; Prawy górny piksel (wiersz powyżej, kolumna r12 + 1)
+    mov     r13, qword ptr [rbp - 8]   ; Ładujemy adres wiersza powyżej
+    mov     r14, r12
+    inc     r14             ; Obliczamy indeks kolumny dla prawego sąsiada
+    movzx   r15d, byte ptr [r13 + r14] ; Wczytujemy wartość piksela z prawej strony
+    imul    r15d, 3         ; Mnożymy przez 3 (waga Scharr dla prawego górnego piksela)
+    sub     dword ptr [rbp - 40], r15d ; Odejmujemy wynik od akumulatora Gx
+    add     dword ptr [rbp - 44], r15d ; Dodajemy wynik do akumulatora Gy
 
-    ; Obliczamy warto�� = |sumX| + |sumY|
-    mov eax, r14d
-    cdq
-    xor eax, edx
-    sub eax, edx
-    mov ebx, r15d
-    cdq
-    xor ebx, edx
-    sub ebx, edx
-    add eax, ebx
-    cmp eax, 255
-    jle StorePixel
-    mov eax, 255
+    ; ----------------- PRZETWARZANIE SĄSIEDZTWA – WIERSZ ŚRODKOWY -----------------
+    ; Lewy piksel bieżący (bieżący wiersz, kolumna r12 - 1)
+    mov     r13, qword ptr [rbp - 16]  ; Ładujemy adres bieżącego wiersza
+    mov     r14, r12
+    dec     r14             ; Obliczamy indeks kolumny dla lewego sąsiada
+    movzx   r15d, byte ptr [r13 + r14] ; Wczytujemy wartość piksela z lewej strony
+    imul    r15d, 5         ; Mnożymy przez 5 (waga Scharr dla lewego piksela)
+    add     dword ptr [rbp - 40], r15d ; Dodajemy wynik do Gx
+    add     dword ptr [rbp - 40], r15d ; Dodajemy jeszcze raz (efektywnie 5 * 2)
 
-StorePixel:
-    ; Obliczamy indeks piksela wyj�ciowego: index = current y * width + current x
-    mov ebx, r10d
-    imul ebx, edi
-    add ebx, r12d
-    mov [rdx+rbx], al
+    ; Prawy piksel bieżący (bieżący wiersz, kolumna r12 + 1)
+    mov     r13, qword ptr [rbp - 16]  ; Ładujemy adres bieżącego wiersza
+    mov     r14, r12
+    inc     r14             ; Obliczamy indeks kolumny dla prawego sąsiada
+    movzx   r15d, byte ptr [r13 + r14] ; Wczytujemy wartość piksela z prawej strony
+    imul    r15d, 5         ; Mnożymy przez 5 (waga Scharr dla prawego piksela)
+    sub     dword ptr [rbp - 40], r15d ; Odejmujemy wynik od Gx
+    sub     dword ptr [rbp - 40], r15d ; Odejmujemy jeszcze raz (efektywnie 5 * 2)
 
-    inc r12d
-    jmp XLoop
+    ; ----------------- PRZETWARZANIE SĄSIEDZTWA – WIERSZ DOLNY -----------------
+    ; Lewy dolny piksel (wiersz poniżej, kolumna r12 - 1)
+    mov     r13, qword ptr [rbp - 24]  ; Ładujemy adres dolnego wiersza
+    mov     r14, r12
+    dec     r14             ; Obliczamy indeks kolumny dla lewego sąsiada
+    movzx   r15d, byte ptr [r13 + r14] ; Wczytujemy wartość piksela
+    imul    r15d, 3         ; Mnożymy przez 3 (waga Scharr dla lewego dolnego piksela)
+    add     dword ptr [rbp - 40], r15d ; Dodajemy wynik do Gx
+    sub     dword ptr [rbp - 44], r15d ; Odejmujemy wynik od Gy
 
-XLoopEnd:
-    inc r10d
-    jmp YLoop
+    ; Dolny środkowy piksel (wiersz poniżej, kolumna r12)
+    mov     r13, qword ptr [rbp - 24]  ; Ładujemy adres dolnego wiersza
+    movzx   r15d, byte ptr [r13 + r12] ; Wczytujemy wartość piksela w kolumnie r12
+    imul    r15d, 5         ; Mnożymy przez 5 (waga Scharr dla środkowego dolnego piksela)
+    sub     dword ptr [rbp - 44], r15d ; Odejmujemy wynik od Gy
+    sub     dword ptr [rbp - 44], r15d ; Odejmujemy jeszcze raz (efektywnie 5 * 2)
 
-LoopEnd:
-    pop r9
-    pop r8
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop rbx
-    pop rdi
-    pop rsi
-    ret
+    ; Prawy dolny piksel (wiersz poniżej, kolumna r12 + 1)
+    mov     r13, qword ptr [rbp - 24]  ; Ładujemy adres dolnego wiersza
+    mov     r14, r12
+    inc     r14             ; Obliczamy indeks kolumny dla prawego sąsiada
+    movzx   r15d, byte ptr [r13 + r14] ; Wczytujemy wartość piksela z prawej strony
+    imul    r15d, 3         ; Mnożymy przez 3 (waga Scharr dla prawego dolnego piksela)
+    sub     dword ptr [rbp - 40], r15d ; Odejmujemy wynik od Gx
+    sub     dword ptr [rbp - 44], r15d ; Odejmujemy wynik od Gy
 
-ExitASM:
-    pop r9
-    pop r8
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop rbx
-    pop rdi
-    pop rsi
-    ret
+    ; ----------------- OBLICZANIE KOLEKTYWNEJ WARTOŚCI GRADIENTU -----------------
+    ; Obliczamy wartość gradientu jako sumę wartości bezwzględnych akumulatorów Gx i Gy
+    mov     eax, dword ptr [rbp - 40]  ; Przenosimy wartość Gx do EAX
+    cmp     eax, 0
+    jge     skip_abs1      ; Jeśli Gx ≥ 0, pomijamy negację
+    neg     eax            ; Jeśli Gx < 0, zmieniamy znak, aby uzyskać wartość bezwzględną
+skip_abs1:
+    mov     edx, dword ptr [rbp - 44]  ; Przenosimy wartość Gy do EDX
+    cmp     edx, 0
+    jge     skip_abs2      ; Jeśli Gy ≥ 0, pomijamy negację
+    neg     edx            ; Jeśli Gy < 0, zmieniamy znak, aby uzyskać wartość bezwzględną
+skip_abs2:
+    add     eax, edx       ; Dodajemy wartości bezwzględne Gx i Gy
+    cmp     eax, 255       ; Sprawdzamy, czy wynik przekracza 255 (maksymalna wartość piksela)
+    jbe     no_clamp       ; Jeśli wynik ≤ 255, nie ograniczamy
+    mov     eax, 255       ; Jeśli wynik > 255, ograniczamy do 255
+no_clamp:
+    ; ----------------- ZAPIS WYNIKU -----------------
+    ; Zapisujemy obliczoną wartość gradientu do odpowiedniego piksela obrazu wyjściowego
+    mov     r13, qword ptr [rbp - 32]  ; Ładujemy adres bieżącego wiersza obrazu wyjściowego
+    mov     byte ptr [r13 + r12], al   ; Zapisujemy wynikowy piksel (wartość w AL) w kolumnie r12
+
+    inc     r12           ; Przechodzimy do następnej kolumny w bieżącym wierszu
+    jmp     inner_loop_start ; Powtarzamy przetwarzanie piksela wewnątrz wiersza
+end_inner_loop:
+    pop     r11           ; Przywracamy zapisany indeks bieżącego wiersza
+    inc     r11           ; Przechodzimy do następnego wiersza
+    jmp     outer_loop    ; Powtarzamy przetwarzanie dla kolejnego wiersza
+end_outer_loop:
+
+    ; -------------------- EPILOG --------------------
+    add     rsp, 64        ; Zwalniamy przydzielony obszar zmiennych lokalnych
+    pop     r15            ; Przywracamy rejestr R15
+    pop     r14            ; Przywracamy rejestr R14
+    pop     r13            ; Przywracamy rejestr R13
+    pop     r12            ; Przywracamy rejestr R12
+    pop     rdi            ; Przywracamy rejestr RDI
+    pop     rsi            ; Przywracamy rejestr RSI
+    pop     rbx            ; Przywracamy rejestr RBX
+    pop     rbp            ; Przywracamy poprzedni wskaźnik ramki stosu
+    ret                   ; Zwracamy kontrolę do wywołującego
 ASMScharrFunction ENDP
 
 END
